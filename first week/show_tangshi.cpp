7eb40cad1858331ade055fc247eeab42b185d281
云宝bao <yunbaobao_bai@qq.com>
2019-03-11 18:45:58 +0800

    show_tangshi over

---
+++ first week/show_tangshi.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	cout << "慈母手中线" << endl;
+ 	cout << "游子身上衣" << endl;
+	cout << "临行密密缝" << endl;
+	cout << "意恐迟迟归" << endl;
+	cout << "谁言寸草心" << endl;
+	cout << "报得三春晖" << endl;
+ 	return 0;
+ }
