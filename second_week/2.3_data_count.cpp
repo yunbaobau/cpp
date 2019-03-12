1d01b53f198840b6b8ea6c8969e50e9834b5f1cb
云宝bao <yunbaobao_bai@qq.com>
2019-03-12 15:50:00 +0800

    2.3 add

---
+++ second_week/2.3_data_count.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	double a,b,c;
+	double m;
+	cin>>a>>b>>c;
+	m=(a+b+c)/3.0;
+	cout<<a+b+c<< endl;
+	cout<<m<<endl;
+	cout<<(int)(m+0.5)<< endl;
+	return 0;
+} 
